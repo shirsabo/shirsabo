f6ea9e22c2aa9ff072a0da9095bf92ba825ade2d
shirsabo <46217802+shirsabo@users.noreply.github.com>
2019-11-23 18:28:14 +0200

    Update ex2.cpp

--- ex2.cpp
+++ ex2.cpp
@@ -20,17 +20,6 @@ public:
         this->obj = objIn;
     }
     node (){
-
-    }
-    void removeFromList(node<type*>* nodeIn) {
-        node<type>* savedPrev = nodeIn->prev;
-        node<type>* savedNext = nodeIn->next;
-        savedPrev->next = savedNext;
-        savedNext->prev = savedPrev;
-    }
-    void moveToHead(node<type*> * nodeIn) {
-        removeFromList(nodeIn);
-        addToFront(nodeIn);
     }
 };
 template <class type> class CacheManager {
@@ -40,20 +29,80 @@ private:
     FILE* disk;
     node <type*>* head;
     node <type*>* tail;
-    unordered_map<string, int> map;
+    unordered_map< string, node<type*>> mp;
 public:
     void insert(string key, type obj) {
-        node<type*>* nodeIn = new ::node<type*>(key,&obj);
+        type* objIn = new type();
+        *objIn = obj;
+        //node <type*> nodeIn= new ::node<type*>(key,new type(obj));
+        node<type*>* nodeIn = new ::node<type*>(key,objIn);
+        auto search = this->mp.find(key);
+        if ( search != this->mp.end()) {
+           node<type*>* temp =  &(search->second);
+           ((search->second).obj)=  objIn;
+         moveToHead(nodeIn,temp);
+            return;
+        }
+        addToFront(nodeIn);
+        put(key,*objIn,nodeIn);
+    }
+    void addToFront(node<type*>* nodeIn) {
+        // Wire up the new node being to be inserted
+        if(this->capacity!=this->totalItemsInCache) {
+            nodeIn->prev = head;
+            nodeIn->next = head->next;
+            auto search = this->mp.find(nodeIn->key);
+            if ( search != this->mp.end()) {
+                (search->second).prev= head;
+                (search->second).next= head->next;
+            }
+            node<type*>* temp =head->next;
+            (head->next)->prev = nodeIn;
+            head->next = nodeIn;
+            search = this->mp.find((temp)->key);
+            if ( search != this->mp.end()) {
+                (search->second).prev = nodeIn;
+            }
+            search = this->mp.find(head->key);
+            if ( search != this->mp.end()) {
+                (search->second).next=nodeIn;
+            }
+            this->totalItemsInCache++;
+        }else {
+            node<type*>* temp = tail->prev;
+            removeFromList(temp);
+            mp.erase(temp->key);
+            addToFront(nodeIn);
+        }
+    }
+    void removeFromList(node<type*>* nodeIn) {
+        node<type*>* savedPrev = nodeIn->prev;
+        node<type*>* savedNext = nodeIn->next;
+        savedPrev->next = savedNext;
+        savedNext->prev = savedPrev;
+        cout<<savedPrev->key;
+        auto search = this->mp.find(savedPrev->key);
+        if ( search != this->mp.end()) {
+            (search->second).next= savedNext;
+        }
+        search = this->mp.find(savedNext->key);
+        if ( search != this->mp.end()) {
+            (search->second).prev= savedPrev;
+        }
+       -- this->totalItemsInCache;
+    }
+    void moveToHead(node<type*> * nodeIn,node<type*>*temp) {
+        removeFromList(temp);
         addToFront(nodeIn);
     }
-    void addToFront(node<type*>* node) {
-        // Wire up the new node being to be inserted
-        node->prev = head;
-        node->next = head->next;
-        head->prev = node;
-        head->next = node;
-    }
     type get(string key) {
+        auto search = mp.find(key);
+        if ( search == mp.end()) {
+            throw " not found"; // we should throw an exception here, but for Leetcode's sake
+        }
+        // Item has been accessed. Move to the front of the cache
+        moveToHead(&(search->second),&(search->second));
+        return (*((search -> second).obj));
     }
     //void foreach(for you to figure out);
     CacheManager(int capacityIn){
@@ -63,40 +112,25 @@ public:
         // Dummy head and tail nodes to avoid empty states
         head = new node<type*>();
         tail = new node<type*>();
+        head->key="HEAD";
+        tail->key="TAIL";
         // Wire the head and tail together
         head->next = tail;
         tail->prev = head;
+        mp.insert({"HEAD",*(head)});
+        mp.insert({"TAIL",*(tail)});
     }
-    void put(string key, type obj) {
-        if (map.find(key) == map.end()) {
-            // Item not found, create a new entry
-            node<type*> * newNode = new node<type*>(key,&obj);
-            newNode->key = key;
-            newNode->value =obj;
-            // Add to the hashtable and the actual list that represents the cache
-            map.insert(key,newNode);
-            addToFront(newNode);
-            totalItemsInCache++;
+    void put(string keyIn, type obj,node<type*>* node1) {
+        auto search = mp.find(keyIn);
+        if ( search== mp.end()) {
+            mp.insert({keyIn,*(node1)});
             // If over capacity remove the LRU item
             if (totalItemsInCache > capacity) {
                 cout<<"overload"<<endl;
             }
         } else {
             // If item is found in the cache, just update it and move it to the head of the list
-
         }
-
     }
-    /*
- type get(int key) {
-        if (node == null) {
-            return -1; // we should throw an exception here, but for Leetcode's sake
-        }
-
-        // Item has been accessed. Move to the front of the cache
-        moveToHead(node);
-
-        return node.value;
-    }*/
 };
 #endif //EX2_EX2_H
