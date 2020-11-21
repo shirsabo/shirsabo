ac7c53eeae89c9d74403e6cc988370433b423f01
shirsabo <46217802+shirsabo@users.noreply.github.com>
2020-11-22 00:34:27 +0200

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,156 +1,191 @@
+import random
 import sys
-import os
-def readfile():
-    print("Processing...")
-    f = open(sys.argv[1], 'r')
-    data = {}
-    for line in f:
-        line = line.strip('\n')
-        x = line.split(",")
-        for i in range(1, len(x)):
-            x[i] = x[i].lstrip(" ")
-            if not x[i] in data:
-                data[x[i]] = {x[0]}
-            else:
-                data[x[i]].add(x[0])
-    f.close()
-    return data
-def output(dictionary):
-    dict={}
-    for key in dictionary:
-       for actor in dictionary[key]:
-            if not actor in dict:
-                dict[actor] = {key}
-            else:
-                dict[actor].add(key)
-    printfile(dict)
-def printfile(dictionary):
-    os.chmod(sys.argv[2], 0o777)
-    f = open(sys.argv[2], 'w')
-    for key in sorted(dictionary.keys()):
-        f.write(key + ',' + ','.join(sorted(dictionary[key])) + '\n')
-    f.close()
-    exit(1)
-def  mainmenu(dictionary):
-    option = 1
-    while option:
-        print("Please select an option:")
-        print("1) Query by movies")
-        print("2) Query by actor")
-        print("3) Insert a new movie")
-        print("4) Save and Exit")
-        print("5) Exit")
-        option = int(input())
-        if option == 1:
-            movies(dictionary)
-        elif option == 2:
-            actors(dictionary)
-        elif option == 3:
-            dictionary = newmovie(dictionary)
-        elif option == 4:
-            output(dictionary)
-        elif option == 5:
-            exit(1)
-def main():
-    dictionary = readfile()
-    mainmenu(dictionary)
-def  checkmovie(movie,dictionary):
-    return movie in dictionary
-def movies(dictionary):
-    union = []
-    print("Please select two movies and an operator(&,|,^) separated with ',':")
-    moviesinput = input()
-    setmovies = moviesinput.split(",")
-    if len(setmovies) < 3:
-        print("Error")
-        return
-    for j in range(0, len(setmovies)):
-        setmovies[j] = setmovies[j].strip()
-    first = checkmovie(setmovies[0], dictionary)
-    second = checkmovie(setmovies[1], dictionary)
-    if (first and second)== False:
-        print("Error")
-        return
-    actorsfirstmovie = set(dictionary[setmovies[0]])
-    actorssecondmovie = set(dictionary[setmovies[1]])
-    if setmovies[2] == "^":
-        xor = actorsfirstmovie ^ actorssecondmovie
-        xor = sorted(xor)
-        if len(xor) == 0:
-            print(" There are no actors in this group")
-            return
-        print(", ".join(xor))
-    elif setmovies[2] == "&":
-        cut = actorsfirstmovie & actorssecondmovie
-        cut = sorted(cut)
-        if len(cut) == 0:
-            print(" There are no actors in this group")
-            return
-        print(", ".join(cut))
-    elif setmovies[2] == "|":
-        union = actorsfirstmovie | actorssecondmovie
-        union = sorted(union)
-        if len(union) == 0:
-            print(" There are no actors in this group")
-            return
-        print(", ".join(union))
-    else:
-        print("Error")
-def actors(dictionary):
-    print("Please select an actor:")
-    players = set()
-    actor = input()
-    for key in dictionary:
-        actors= dictionary[key]
-        if actor in actors:
-            l = set(dictionary[key])
-            players = players | l
-    if not players:
-        print ("Error")
-        return
-    players.remove(actor)
-    if len(players) == 0:
-        print("There are no actors in this group")
-        return
-    sortedplayers = sorted(set(players))
-    print(", ".join(sortedplayers))
-def newmovie(dictionary):
-    print("Please insert a new movie:")
-    new = input()
-    new = new.split(",")
-    lenght = len(new)
-    if lenght <= 2:
-        print("Error")
-        return dictionary
-    for j in range(0, len(new)):
-        new[j] = new[j].lstrip(' ')
-    movie = new[0]
-    new = set(new)
-    new.remove(movie)
-    for key in dictionary:
-        if key == movie:
-            current = dictionary[key]
-            new = current | new
-            dictionary[key] = new
-    dictionary[movie] = new
-    return dictionary
+import numpy as np
+import scipy.io.wavfile
+from sklearn.utils import shuffle
 
-if __name__ == "__main__":
-     main()
+def create_matrix():
+    arr = []
+    for i in range (3):
+        arr.append(np.append(np.random.uniform(0, 1, 12),0))
+    return arr
+
+def preceptron_accuracy(xlist,ylist):
+    n = len(xlist)
+    size = round(n * 0.8)
+    linetrainX = xlist[:size]
+    linetrainy = ylist[:size]
+    testx = xlist[size:]
+    testy = ylist[size:]
+    loss = 0
+    matrix = preceptron_algo(linetrainX,linetrainy)
+    for i in range (len(testx)):
+        y_hat = np.argmax(np.dot(matrix,np.append(testx[i],1)))
+        print (y_hat,int(testy[i]))
+        if int(testy[i]) != y_hat:
+            loss  = loss +1
+    print("preceptron_accuracy")
+    print (loss)
+
+def preceptron_algo(pxlist,pylist):
+    ephocs = 13
+    learningRate = 1
+    eta = np.array([learningRate]*13)
+    eta [12] = 1
+    matrix = create_matrix()
+    for e in range(ephocs):
+        Xconvert = []
+        x_train, y_train = shuffle(pxlist, pylist, random_state=1)
+        for i in range(0,len(pxlist)):
+            print (i)
+            Xconvert.append(np.append(x_train[i], 1))
+        x_train = Xconvert
+        for x, y in zip(x_train, y_train):
+            y_hat = np.argmax(np.dot(matrix, x))
+            if y != y_hat:
+                matrix[int(y)] = matrix[int(y_hat)] + learningRate * x
+                matrix[int(y)][len(matrix[int(y)])-1] = matrix[int(y)][len(matrix[int(y)])-1] + 1
+                matrix[int(y_hat)] = matrix[int(y_hat)] - learningRate * x
+                matrix[int(y_hat)][len(matrix[int(y_hat)]) - 1] = matrix[int(y_hat)][len(matrix[int(y_hat)]) - 1] - 1
+    print (matrix)
+    print("--------------------")
+    return matrix
+
+#-----------------------------------------------
+def norm_vec(Xlist, testIn, flag):
+    if flag == 1:
+        tests = TestList()
+    elif flag == 0:
+        tests = testIn
+    minVal, maxVal = min_max(Xlist)
+    y = np.array(np.concatenate((tests, Xlist.copy())))
+    for i in range(0, len(Xlist)):
+        for j in range(0, len(minVal)):
+            # print (i,j)
+            Xlist[i][j] = (Xlist[i][j] - minVal[j]) / (maxVal[j] - minVal[j])
+    for testElement in range(i, len(y)):
+        for col in range(0, len(minVal)):
+            if (testElement - i - 1 >= 0):
+                tests[testElement - i - 1][col] = (tests[testElement - i - 1][col] - minVal[col]) / (
+                            maxVal[col] - minVal[col])
+    return Xlist, tests
 
 
+def min_max(Xlist):
+    n = len(Xlist)
+    m = len(Xlist[0])
+    matrix = []
+    for i in range(n):
+        matrix.append(np.asarray(Xlist[i]).astype(np.double))
+    cols_num = len(matrix[0])
+    min_vals, max_vals = [0] * m, [0] * m
+    for y in range(0, m):
+        val = matrix[0][y]
+        min_vals[y], max_vals[y] = val, val
+        for x in range(0, n):
+            if matrix[x][y] > max_vals[y]:
+                max_vals[y] = matrix[x][y]
+            if matrix[x][y] < min_vals[y]:
+                min_vals[y] = matrix[x][y]
+    return min_vals, max_vals
 
 
+def validation(line1x, line1y):
+    n = len(line1x)
+    size = round(n * 0.8)
+    dic1 = {}
+    for i in range(size):
+        dic1.setdefault(line1y[i].replace('\n', ''), []).append(np.asarray(line1x[i]).astype(np.double))
+    linetrain = line1x[:size]
+    line1x = line1x[size:]
+    linetrain, line1x = norm_vec(linetrain, line1x, 0)
+    line1y = line1y[size:]
+    loss = 0;
+    iindex = 0
+    minLoss = -1;
+    dictemp = dict(dic1)
+    for k in range(1, 20):
+        loss = 0
+        for e in range(0, len(line1x)):
+            # print (k,e )
+            predict = k_nearest_neighbors(dictemp, dic1, np.asarray(line1x[e]).astype(np.double), k)
+            if predict != line1y[e].replace('\n', ''):
+                loss = loss + 1
+        print(loss)
+        if (loss / len(line1x)) <= 0.2:
+            print(loss / len(line1x))
+            return k
 
 
+def TestList():
+    testList = []
+    with open(test, 'r') as readerTest:
+        testVec = readerTest.readline()
+        while testVec != '':
+            testVec = convertVec(testVec.split(","))
+            testList.append(np.asarray(testVec).astype(np.float))
+            testVec = readerTest.readline()
+    return testList
 
 
+def KNN_algo(dic2, listTest, k):
+    # print (len(testList))
+
+    for i in range(len(listTest)):
+        predict = k_nearest_neighbors(dic2, dic2, np.asarray(listTest[i]).astype(np.double), k)
+        print("knn: " + predict)
 
 
+def convertVec(x1):
+    if x1[11][0] == 'W':
+        x1[11] = "1"
+    # x1.append("0")
+    elif x1[11][0] == 'R':
+        x1[11] = "0"
+    # x1.append("1")
+    return x1
 
 
+def most_frequent(List):
+    return str(np.bincount(List).argmax())
 
 
+def euclidianDistance(self, a, b):
+    return np.sqrt(np.sum((a - b) ** 2, axis=1))
 
 
+def k_nearest_neighbors(tempData, data, predict, k):
+    distances = []
+    for key, value in tempData.items():
+        for vec in value:
+            euclidean_distance = np.linalg.norm(vec - predict)
+            distances.append([euclidean_distance, key])
+    votes = [i[1] for i in sorted(distances)[:k]]
+    # votes.sort(reverse=True)
+    vote_result = most_frequent(votes)
+    return vote_result
 
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test, train_x, train_y = sys.argv[1], sys.argv[2], sys.argv[3]
+    xList, yList = [], []
+    dic = {}
+    Xlist, Ylist = [], []
+    with open(train_x, 'r') as readerX:
+        with open(train_y, 'r') as readerY:
+            lineX, lineY = readerX.readline(), readerY.readline()
+            while lineX != '' and lineY != '':  # The EOF char is an empty string
+                x1 = lineX.split(",")
+                x1 = convertVec(x1)
+                Xlist.append(np.asarray(x1).astype(np.double))
+                Ylist.append(lineY)
+                # dic[lineY.replace('\n', '')] = np.asarray(x1).astype(np.double)
+                lineX = readerX.readline()
+                lineY = readerY.readline()
+    Xlist, testList = norm_vec(np.asarray(Xlist), None, 1)
+    for i in range(len(Xlist)):
+        dic.setdefault(Ylist[i].replace('\n', ''), []).append(Xlist[i])
+    KNN_algo(dic, testList, validation(Xlist, Ylist))
+    preceptron_accuracy(Xlist, Ylist)
+    # KNN_algo(dic,testList,4)
